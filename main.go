a031ad5361f7432a90351fe5e6e331e3c8a9034d
Pankajvl <pankaj.vl@zop.dev>
2025-06-16 17:03:40 +0530

    Changes done

--- main.go
+++ main.go
@@ -9,5 +9,5 @@ func main() {
 	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
 	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
 
-	fmt.Println("Hello World hi"
+	fmt.Println("Hello World hi")
 }
